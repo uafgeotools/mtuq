7212bdcec80d0ac73dfd4eb6c78faeb6ba8a9b9f
Ryan Modrak <rmodrak@gmail.com>
2017-11-13 08:16:03 -0900

    Updated script

--- prototype/GridSearch6Parameter.py
+++ prototype/GridSearch6Parameter.py
@@ -11,32 +11,33 @@ from mtuq.grid_search import Grid, UniformGrid, grid_search
 
 if __name__ == '__main__':
     # event information
-    path = ''
-    name = ''
+    path = 'dummy_path'
+    name = 'dummy_name'
     data_format = 'sac'
 
-    # define data categories and data processing functions
-    process_data {
+    # read data
+    data = mtuq.io.read(path, data_format)
+    origin = mtuq.io.get_origin(data, data_format)
+    stations = mtuq.io.get_stations(data, data_format)
+
+    # define data categories and processing functions
+    process_data = {
        'bw': process_bw_factory(Tmin=20., Tmax=40.),
        'sw': process_sw_factory(Tmin=60., Tmax=120.),
        }
 
     # define data misfit
-    misfit = mtuq.misfit_functions('waveform_difference',
-        normalize='L2',
-        station_correction='cc')
+    misfit = mtuq.misfit.waveform_difference_cc
 
     # define Green's functions generater
-    gf_generater = mtuq.synthetics1d.fk_factory(path='')
+    model = 'dummy_model'
+    gf_generater = mtuq.synthetics1d.fk_factory(model)
 
-    # read data
-    data = mtuq.io.read(path, format=data_format)
-    origin = mtuq.io.get_origin(data, format=data_format)
-    stations = mtuq.io.get_stations(data, format=data_format)
+    # read precomputed Greens functions
+    green_functions = gf_generator(data, origin, stations)
+    convolved_gf = convolve_source_wavelet(greens_functions, wavelet)
 
-    # preload Greens functions
-    green_functions = generator(data, origin, stations)
-    convolved = convolve_source_wavelet(greens_functions, wavelet)
+    # resample Greens functions
 
     # process traces
     categories = process_data.keys()
@@ -44,7 +45,7 @@ if __name__ == '__main__':
     processed_greens_functions = {}
     for key in categories:
         processed_data[key] = process_data[key](data)
-        processed_greens_functions[key] = process_data[key](convolved_greens_functions)
+        processed_gf[key] = process_data[key](convolved_gf)
 
     # define moment tensor grid
     mt_type = 'Tape2015'
@@ -52,12 +53,13 @@ if __name__ == '__main__':
         'v': [vmin, vmax, nv],
         'w': [wmin, wmax, nw],
         'h': [hmin, hmax, nw],
-        'delta': [min, max, n],
-        'theta': [min, max, n],
-        'kappa': [min, max, n],
+        'delta': [dmin, dmax, n],
+        'theta': [tmin, tmax, n],
+        'kappa': [kmin, kmax, n],
         })
     grid = MTGridUniform(mt_type, mt_bounds)
 
     # carry out grid search in parallel
-    grid_search_mt(processed_data, processed_greens_functions, misfit, grid)
+    grid_search_mt_mpi(processed_data, processed_gf, misfit, grid)
+
 
