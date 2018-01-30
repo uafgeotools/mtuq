94f8b01fbb498daf408b5aac0103682c486e6ec2
Ryan Modrak <rmodrak@gmail.com>
2018-01-30 00:33:24 -0900

    Updated utilities

--- mtuq/util/geodetics.py
+++ mtuq/util/geodetics.py
@@ -6,7 +6,7 @@ from obspy.geodetics import gps2dist_azimuth
 def distance(station, origin):
     if hasattr(station, 'latitude') and\
         hasattr(station, 'longitude'):
-        dist, azim, bazim = gps2dist_azimuth(
+        dist, _, _, = gps2dist_azimuth(
             station.latitude,
             station.longitude,
             origin.latitude,
@@ -15,3 +15,18 @@ def distance(station, origin):
 
     else:
         raise Exception
+
+
+def distance_azimuth(station, origin):
+    if hasattr(station, 'latitude') and\
+        hasattr(station, 'longitude'):
+        dist, azim, _, = gps2dist_azimuth(
+            station.latitude,
+            station.longitude,
+            origin.latitude,
+            origin.longitude)
+        return dist/1000., azim
+
+    else:
+        raise Exception
+
--- mtuq/util/util.py
+++ mtuq/util/util.py
@@ -8,3 +8,18 @@ class AttribDict(dict):
         self.__dict__ = self
 
 
+class Struct(dict):
+    """ Dictionary with both keyword and attribute access
+    """
+    def __init__(self, *args, **kwargs):
+        super(Struct, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def iterable(arg):
+    if not isinstance(arg, (list, tuple)):
+        return [arg]
+    else:
+        return arg
+
+
+
