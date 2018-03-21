893977a1c463e707209d273528aea7b7a685fbfd
Ryan Modrak <rmodrak@gmail.com>
2018-03-21 15:37:13 -0800

    Pushed for greater similarity between Dataset, GreensTensor

--- mtuq/dataset/base.py
+++ mtuq/dataset/base.py
@@ -1,7 +1,7 @@
 
 import obspy
 
-class Dataset(object):
+class DatasetBase(object):
 
     def __init__(self, data=None, id=None):
         # event name or other unique identifier
@@ -15,12 +15,26 @@ class Dataset(object):
             self.__add__(stream)
 
 
+    def get_origin(self):
+        """
+        Extracts origin information from metadata
+        """
+        raise NotImplementedError
+
+
+    def get_stations(self):
+        """
+        Extracts station information from metadata
+        """
+        raise NotImplementedError
+
+
     def apply(self, function, *args, **kwargs):
         """
         Returns the result of applying a function to each Stream in the 
         list. Similar to the behavior of the python built-in "apply".
         """
-        processed = Dataset(id=self.id)
+        processed = self.__class__(id=self.id)
         for stream in self.__list__:
             processed += function(stream, *args, **kwargs)
         return processed
@@ -33,7 +47,7 @@ class Dataset(object):
         called with an argument list consisting of the corresponding item of
         each sequence. Similar to the behavior of the python built-in "map".
         """
-        processed = Dataset(id=self.id)
+        processed = self.__class__(id=self.id)
         for _i, stream in enumerate(self.__list__):
             args = [sequence[_i] for sequence in sequences]
             processed += function(stream, *args)
