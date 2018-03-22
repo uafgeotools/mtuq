45e8a2808851381343a4a970f71d7ec4bef54231
Ryan Modrak <rmodrak@gmail.com>
2018-03-22 00:34:48 -0800

    Updated examples

--- examples/DCGridSearch3Parameter.py
+++ examples/DCGridSearch3Parameter.py
@@ -2,8 +2,8 @@
 import os
 import sys
 import numpy as np
-import mtuq.io
-import mtuq.greens.fk
+import mtuq.dataset.sac
+import mtuq.greens_tensor.fk
 import mtuq.misfit
 
 from os.path import basename, join
@@ -16,74 +16,71 @@ from mtuq.util.util import Struct, root
 from mtuq.util.wavelets import trapezoid
 
 
-# eventually we need to include sample data in the mtuq repository;
-# file size prevents us from doing so right away
-paths = Struct({
-    'data': join(root(), 'tests/data/20090407201255351'),
-   #'weights': join(root(), 'tests/data/20090407201255351/weight.dat'),
-    'weights': join(root(), 'tests/data/20090407201255351/weight_test.dat'),
-    'greens': os.getenv('CENTER1')+'/'+'data/wf/FK_SYNTHETICS/scak',
-    })
-
-data_format = 'sac'
-event_name = basename(paths.data)
-
-
-# body and surface waves are processed separately
-process_bw = process_data(
-    filter_type='Bandpass',
-    freq_min= 0.25,
-    freq_max= 0.667,
-    window_length=15.,
-    #window_type='cap_bw',
-    weight_type='cap_bw',
-    weight_file=paths.weights,
-    )
-
-process_sw = process_data(
-    filter_type='Bandpass',
-    freq_min=0.025,
-    freq_max=0.0625,
-    window_length=150.,
-    #window_type='cap_sw',
-    weight_type='cap_sw',
-    weight_file=paths.weights,
-    )
-
-process_data = {
-   'body_waves': process_bw,
-   'surface_waves': process_sw,
-   }
-
-
-# total misfit is a sum of body- and surface-wave contributions
-misfit_bw = cap_bw(
-    max_shift=2.,
-    )
-
-misfit_sw = cap_sw(
-    max_shift=10.,
-    )
-
-misfit = {
-    'body_waves': misfit_bw,
-    'surface_waves': misfit_sw,
-    }
-
-# search over 50,000 randomly-chosen double-couple moment tensors
-grid = DCGridRandom(
-    npts=50000,
-    Mw=4.5,
-    )
-
-
 if __name__=='__main__':
     """ Carries out grid search over double-couple moment tensor parameters,
        keeping magnitude, depth, and location fixed
     """
-    data = mtuq.io.read(data_format, paths.data, wildcard='*.[zrt]')
-    origin = mtuq.io.get_origin(data_format, data)
-    stations = mtuq.io.get_stations(data_format, data)
+
+    # eventually we need to include sample data in the mtuq repository;
+    # file size prevents us from doing so right away
+    paths = Struct({
+        'data': join(root(), 'tests/data/20090407201255351'),
+        'weights': join(root(), 'tests/data/20090407201255351/weight_test.dat'),
+        'greens': os.getenv('CENTER1')+'/'+'data/wf/FK_SYNTHETICS/scak',
+        })
+
+    # body and surface waves are processed separately
+    process_bw = process_data(
+        filter_type='Bandpass',
+        freq_min= 0.25,
+        freq_max= 0.667,
+        window_length=15.,
+        #window_type='cap_bw',
+        weight_type='cap_bw',
+        weight_file=paths.weights,
+        )
+
+    process_sw = process_data(
+        filter_type='Bandpass',
+        freq_min=0.025,
+        freq_max=0.0625,
+        window_length=150.,
+        #window_type='cap_sw',
+        weight_type='cap_sw',
+        weight_file=paths.weights,
+        )
+
+    process_data = {
+       'body_waves': process_bw,
+       'surface_waves': process_sw,
+       }
+
+
+    # total misfit is a sum of body- and surface-wave contributions
+    misfit_bw = cap_bw(
+        max_shift=2.,
+        )
+
+    misfit_sw = cap_sw(
+        max_shift=10.,
+        )
+
+    misfit = {
+        'body_waves': misfit_bw,
+        'surface_waves': misfit_sw,
+        }
+
+    # search over randomly-chosen double-couple moment tensors
+    grid = DCGridRandom(
+        npts=50,
+        Mw=4.5,
+        )
+
+    print 'Reading data...\n'
+    data = mtuq.dataset.sac.reader(paths.data, wildcard='*.[zrt]')
+    origin = data.get_origin()
+    stations = data.get_stations()
+    event_name = data.id
 
     print 'Processing data...\n'
     processed_data = {}
@@ -92,19 +89,23 @@ if __name__=='__main__':
     data = processed_data
 
     print 'Reading Greens functions...\n'
-    generator = mtuq.greens.fk.Generator(paths.greens)
+    generator = mtuq.greens_tensor.fk.Generator(paths.greens)
     greens = generator(stations, origin)
-    #greens.convolve(trapezoid(rise_time=1.))
 
     print 'Processing Greens functions...\n'
+    #greens.convolve(trapezoid(rise_time=1.))
     processed_greens = {}
     for key in process_data:
         processed_greens[key] = greens.map(process_data[key], stations)
     greens = processed_greens
 
     print 'Carrying out grid search...\n'
-    mt = grid_search_serial(data, greens, misfit, grid)
+    results = grid_search_serial(data, greens, misfit, grid)
 
-    print 'Plotting results...'
-    cap_plot(event_name+'.png', data, greens, mt, paths.weights)
+    print 'Saving results...\n'
+    grid.save(data.id+'.h5', {'misfit': results})
+
+    print 'Plotting waveforms...\n'
+    mt = grid.get(results.argmin())
+    cap_plot(data.id+'.png', data, greens, mt, paths.weights)
 
