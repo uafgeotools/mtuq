05923d16c60e2ecc92bca5b0eebcabab71adb6d7
Ryan Modrak <rmodrak@gmail.com>
2018-05-25 13:52:06 -0800

    Variable name changes

--- mtuq/greens_tensor/syngine.py
+++ mtuq/greens_tensor/syngine.py
@@ -32,7 +32,7 @@ SYNGINE_FILENAMES = [
 
 # If a GreensTensor is created with the wrong input arguments, this error
 # message is displayed.  In practice this is rarely encountered, since
-# Generator normally does all the work
+# GreensTensorFactory normally does all the work
 ErrorMessage=''
 
 
@@ -42,7 +42,7 @@ class GreensTensor(mtuq.greens_tensor.instaseis.GreensTensor):
 
 
 
-class Generator(mtuq.greens_tensor.base.Generator):
+class GreensTensorFactory(mtuq.greens_tensor.base.GreensTensorFactory):
     def __init__(self, model):
         self.model = model
 
@@ -54,8 +54,10 @@ class Generator(mtuq.greens_tensor.base.Generator):
         distance = km2deg(station.distance)
         depth = origin.depth
 
-        # download, unizp, and read
-        dirname = unzip(download(model, delta, distance, depth))
+        # download and unizp data
+        dirname = unzip(download_greens_functions(model, delta, distance, depth))
+
+        # read data
         stream = Stream()
         stream.id = station.id
         for filename in SYNGINE_FILENAMES:
@@ -71,6 +73,9 @@ class Generator(mtuq.greens_tensor.base.Generator):
         t2_old = float(origin.time)+float(stream[0].stats.endtime)
         dt_old = float(stream[0].stats.delta)
 
+        print(t1_old, t2_old, dt_old, t1_new, t2_new, dt_new)
+            
+
         for trace in stream:
             # resample Green's functions
             data_old = trace.data
@@ -83,8 +88,8 @@ class Generator(mtuq.greens_tensor.base.Generator):
         return GreensTensor(stream, station, origin)
 
 
-def download(model, delta, distance_in_deg, depth_in_m):
-    """ Downloads Green's function through syngine URL interface
+def download_greens_functions(model, delta, distance_in_deg, depth_in_m):
+    """ Downloads Green's functions through syngine URL interface
     """
     url = ('http://service.iris.edu/irisws/syngine/1/query'
          +'?model='+model
@@ -98,6 +103,33 @@ def download(model, delta, distance_in_deg, depth_in_m):
          +'&greensfunction=1'
          +'&sourcedistanceindegrees='+str(distance_in_deg)
          +'&sourcedepthinmeters='+str(int(round(depth_in_m)))
+         +'.zip')
+    download = urllib.URLopener()
+    download.retrieve(url, filename)
+    return filename
+
+
+def download_synthetics(mt, model, delta, station, origin):
+    """ Downloads synthetics through syngine URL interface
+    """
+    url = ('http://service.iris.edu/irisws/syngine/1/query'
+         +'?model='+model
+         +'&dt='+str(delta)
+         +'&receiverlatitude'+str(station.latitude)
+         +'&receiverlongitude'+str(station.longitude)
+         +'&sourclatitude'+str(origin.latitude)
+         +'&sourcelongitude'+str(origin.longitude)
+         +'&sourcedepthinmeters='+str(int(round(origin.depth)))
+         +'&sourcemomenttensor='+",".join(map(str, mt)))
+    filename = ('tmp-'
+         +'model='+model
+         +'dt='+str(delta)
+         +'&receiverlatitude'+str(station.latitude)
+         +'&receiverlongitude'+str(station.longitude)
+         +'&sourclatitude'+str(origin.latitude)
+         +'&sourcelongitude'+str(origin.longitude)
+         +'&sourcedepthinmeters='+str(int(round(origin.depth)))
+         +'&sourcemomenttensor='+",".join(map(str, mt))
          +'.zip')
     download = urllib.URLopener()
     download.retrieve(url, filename)
