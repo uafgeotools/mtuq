26d3ef35961388662bc0ab03539c5becf4636a81
Ryan Modrak <rmodrak@gmail.com>
2018-07-02 16:09:32 -0800

    Implemented check_examples.bash

---
+++ tests/check_examples.bash
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+
+#
+# Checks whether code generater output matches examples included in repository
+#
+
+
+# navigate to mtuq/tests
+cd $(dirname ${BASH_SOURCE[0]})
+
+for filename in \
+    ../examples/GridSearch.DoubleCouple.3Parameter.MPI.py\
+    ../examples/GridSearch.DoubleCouple.3Parameter.Serial.py;
+do
+    echo "Checking $filename..."
+    cp ${filename}{,~}
+
+    python ../data/examples/code_generator.py
+    diff ${filename}{,~}
+
+    rm ${filename}~
+done
+
