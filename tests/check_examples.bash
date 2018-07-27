43ef069714492335d662574714c052c258624c3f
Ryan Modrak <rmodrak@gmail.com>
2018-07-26 16:15:55 -0800

    Fixed bug in check_examples

--- tests/check_examples.bash
+++ tests/check_examples.bash
@@ -2,24 +2,28 @@
 
 
 #
-# Checks whether code generater output matches examples included in repository
+# Checks whether existing files match code generater output
 #
 
+FILENAMES="\
+    ../examples/GridSearch.DoubleCouple.3Parameter.MPI.py\
+    ../examples/GridSearch.DoubleCouple.3Parameter.Serial.py\
+    ../tests/benchmark_cap_fk.py\
+    "
 
 # navigate to mtuq/tests
 cd $(dirname ${BASH_SOURCE[0]})
 
-for filename in \
-    ../examples/GridSearch.DoubleCouple.3Parameter.MPI.py\
-    ../examples/GridSearch.DoubleCouple.3Parameter.Serial.py\
-    ../tests/benchmark_cap_fk.py;
+for filename in $FILENAMES
 do
     echo "Checking $filename..."
     cp ${filename}{,~}
+done
 
-    python ../data/examples/code_generator.py
-    diff ${filename}{,~}
-
+python ../data/examples/code_generator.py
+for filename in $FILENAMES
+do
+    cmp ${filename}{,~}
     rm ${filename}~
 done
 echo "SUCCESS"
