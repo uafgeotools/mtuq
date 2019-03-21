295d2f697f8c8c2f7e012568284e236613c255ee
Ryan <rmodrak@gmail.com>
2019-03-21 13:59:02 -0400

    Created setup/chinook/examples directory

---
+++ setup/chinook/examples/CapStyleGridSearch.DoubleCouple.py
@@ -0,0 +1,162 @@
+
+import os
+import sys
+import numpy as np
+
+from os.path import join
+from mtuq import read, get_greens_tensors, open_db
+from mtuq.grid import DoubleCoupleGridRandom
+from mtuq.grid_search.mpi import grid_search_mpi
+from mtuq.cap.misfit import Misfit
+from mtuq.cap.process_data import ProcessData
+from mtuq.cap.util import Trapezoid
+from mtuq.util.plot import plot_beachball, plot_data_greens_mt
+from mtuq.util.util import path_mtuq
+
+
+
+if __name__=='__main__':
+    #
+    # CAP-style Double-couple inversion example
+    # 
+    # Carries out grid search over 50,000 randomly chosen double-couple 
+    # moment tensors
+    #
+    # USAGE
+    #   mpirun -n <NPROC> python CapStyleGridSearch.DoubleCouple.py
+    #
+
+
+    path_data=    join(path_mtuq(), 'data/examples/20090407201255351/*.[zrt]')
+    path_weights= join(path_mtuq(), 'data/examples/20090407201255351/weights.dat')
+    path_picks=   join(path_mtuq(), 'data/examples/20090407201255351/picks.dat')
+    event_name=   '20090407201255351'
+    model=        'scak'
+
+
+    process_bw = ProcessData(
+        filter_type='Bandpass',
+        freq_min= 0.1,
+        freq_max= 0.333,
+        pick_type='from_pick_file',
+        pick_file=path_picks,
+        window_type='cap_bw',
+        window_length=15.,
+        padding_length=2.,
+        weight_type='cap_bw',
+        cap_weight_file=path_weights,
+        )
+
+    process_sw = ProcessData(
+        filter_type='Bandpass',
+        freq_min=0.025,
+        freq_max=0.0625,
+        pick_type='from_pick_file',
+        pick_file=path_picks,
+        window_type='cap_sw',
+        window_length=150.,
+        padding_length=10.,
+        weight_type='cap_sw',
+        cap_weight_file=path_weights,
+        )
+
+    process_data = {
+       'body_waves': process_bw,
+       'surface_waves': process_sw,
+       }
+
+
+    misfit_bw = Misfit(
+        time_shift_max=2.,
+        time_shift_groups=['ZR'],
+        )
+
+    misfit_sw = Misfit(
+        time_shift_max=10.,
+        time_shift_groups=['ZR','T'],
+        )
+
+    misfit = {
+        'body_waves': misfit_bw,
+        'surface_waves': misfit_sw,
+        }
+
+
+    #
+    # Next we specify the source parameter grid
+    #
+
+    grid = DoubleCoupleGridRandom(
+        npts=50000,
+        moment_magnitude=4.5)
+
+    wavelet = Trapezoid(
+        moment_magnitude=4.5)
+
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+
+
+    #
+    # The main I/O work starts now
+    #
+
+    if comm.rank==0:
+        print 'Reading data...\n'
+        data = read(path_data, format='sac', 
+            event_id=event_name,
+            tags=['units:cm', 'type:velocity']) 
+
+        data.sort_by_distance()
+
+        stations = data.get_stations()
+        origin = data.get_origin()
+
+        print 'Processing data...\n'
+        processed_data = {}
+        for key in ['body_waves', 'surface_waves']:
+            processed_data[key] = data.map(process_data[key])
+        data = processed_data
+
+        print 'Reading Greens functions...\n'
+        greens = get_greens_tensors(stations, origin, model=model)
+
+        print 'Processing Greens functions...\n'
+        greens.convolve(wavelet)
+        processed_greens = {}
+        for key in ['body_waves', 'surface_waves']:
+            processed_greens[key] = greens.map(process_data[key])
+        greens = processed_greens
+
+    else:
+        data = None
+        greens = None
+
+    data = comm.bcast(data, root=0)
+    greens = comm.bcast(greens, root=0)
+
+
+    #
+    # The main computational work starts now
+    #
+
+    if comm.rank==0:
+        print 'Carrying out grid search...\n'
+    results = grid_search_mpi(data, greens, misfit, grid)
+    results = comm.gather(results, root=0)
+
+
+    if comm.rank==0:
+        print 'Saving results...\n'
+        results = np.concatenate(results)
+        #grid.save(event_name+'.h5', {'misfit': results})
+        best_mt = grid.get(results.argmin())
+
+
+    if comm.rank==0:
+        print 'Plotting waveforms...\n'
+        plot_data_greens_mt(event_name+'.png', data, greens, best_mt, misfit)
+        plot_beachball(event_name+'_beachball.png', best_mt)
+
+
