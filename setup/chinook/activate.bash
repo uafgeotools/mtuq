ace9344a4d93518558f7f00e9bf059246c8e2916
Ryan <rmodrak@gmail.com>
2019-03-21 11:33:17 -0400

    Added chinook install scripts

---
+++ setup/chinook/activate.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Activates MTUQ virtual environment on chinook
+#
+# Invoke this script by sourcing it, i.e.
+# > source activate.bash
+
+
+ERROR="
+This script is not being sourced
+"
+if [ "${BASH_SOURCE[0]}" == "${0}" ];
+then
+    echo $ERROR
+    exit 1
+fi
+
+
+ERROR="
+This script works only on chinook.alaska.edu
+"
+if [[ $HOSTNAME != chinook* ]];
+then
+    echo "This script works only on chinook.alaska.edu"
+    return 1
+fi
+
+
+# what is the absolute path of mtuq/setup/chinook?
+SETUP=$(dirname ${BASH_SOURCE[0]})
+
+
+ERROR="
+Virtual environment not found.
+Run mtuq/setup/chinook/install.bash and try again.
+"
+if [[ -d $SETUP/virtual/mtuq  ]];
+then
+    echo "$ERROR"
+    return 1
+fi
+
+
+# load system modules
+module load lang/Python/2.7.12-pic-intel-2016b
+
+
+# activate virutal environment
+source $SETUP/virtual/$VENV/mtuq/bin/activate
+
+
