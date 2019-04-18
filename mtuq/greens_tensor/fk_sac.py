d8662096253191db084618a3ee4c846654d9c91a
Ryan Modrak <rmodrak@gmail.com>
2019-04-18 01:40:27 -0800

    Reverted previous indexing changes

--- mtuq/greens_tensor/fk_sac.py
+++ mtuq/greens_tensor/fk_sac.py
@@ -65,34 +65,34 @@ class GreensTensor(GreensTensorBase):
                 ZDS = self.select(channel="ZDS")[0].data
                 ZDD = self.select(channel="ZDD")[0].data
                 ZEP = self.select(channel="ZEP")[0].data
-                array[_i, :, 0] = -ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEP/3.
-                array[_i, :, 1] =  ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEP/3.
-                array[_i, :, 2] =  ZDD/3. + ZEP/3.
-                array[_i, :, 3] = -ZSS * np.sin(2*phi)
-                array[_i, :, 4] = -ZDS * np.cos(phi)
-                array[_i, :, 5] = -ZDS * np.sin(phi)
+                array[_i, 0, :] = -ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEP/3.
+                array[_i, 1, :] =  ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEP/3.
+                array[_i, 2, :] =  ZDD/3. + ZEP/3.
+                array[_i, 3, :] = -ZSS * np.sin(2*phi)
+                array[_i, 4, :] = -ZDS * np.cos(phi)
+                array[_i, 5, :] = -ZDS * np.sin(phi)
 
             elif component=='R':
                 RSS = self.select(channel="RSS")[0].data
                 RDS = self.select(channel="RDS")[0].data
                 RDD = self.select(channel="RDD")[0].data
                 REP = self.select(channel="REP")[0].data
-                array[_i, :, 0] = -RSS/2. * np.cos(2*phi) - RDD/6. + REP/3.
-                array[_i, :, 1] =  RSS/2. * np.cos(2*phi) - RDD/6. + REP/3.
-                array[_i, :, 2] =  RDD/3. + REP/3.
-                array[_i, :, 3] = -RSS * np.sin(2*phi)
-                array[_i, :, 4] = -RDS * np.cos(phi)
-                array[_i, :, 5] = -RDS * np.sin(phi)
+                array[_i, 0, :] = -RSS/2. * np.cos(2*phi) - RDD/6. + REP/3.
+                array[_i, 1, :] =  RSS/2. * np.cos(2*phi) - RDD/6. + REP/3.
+                array[_i, 2, :] =  RDD/3. + REP/3.
+                array[_i, 3, :] = -RSS * np.sin(2*phi)
+                array[_i, 4, :] = -RDS * np.cos(phi)
+                array[_i, 5, :] = -RDS * np.sin(phi)
 
             elif component=='T':
                 TSS = self.select(channel="TSS")[0].data
                 TDS = self.select(channel="TDS")[0].data
-                array[_i, :, 0] = -TSS/2. * np.sin(2*phi)
-                array[_i, :, 1] =  TSS/2. * np.sin(2*phi)
-                array[_i, :, 2] =  0.
-                array[_i, :, 3] =  TSS * np.cos(2*phi)
-                array[_i, :, 4] = -TDS * np.sin(phi)
-                array[_i, :, 5] =  TDS * np.cos(phi)
+                array[_i, 0, :] = -TSS/2. * np.sin(2*phi)
+                array[_i, 1, :] =  TSS/2. * np.sin(2*phi)
+                array[_i, 2, :] =  0.
+                array[_i, 3, :] =  TSS * np.cos(2*phi)
+                array[_i, 4, :] = -TDS * np.sin(phi)
+                array[_i, 5, :] =  TDS * np.cos(phi)
 
             else:
                 raise ValueError
