821b244fa8f3180f1c18aa63925a3a07f1f174d4
Ryan <rmodrak@gmail.com>
2019-07-17 18:09:42 -0800

    Fixed minor regressions

--- tests/check_import.py
+++ tests/check_import.py
@@ -27,6 +27,7 @@ failed = []
 excluded += ['mtuq.grid_search._extensions']
 excluded += ['mtuq.grid_search.mpic']
 excluded += ['mtuq.grid_search.serialc']
+excluded += ['mtuq.util.examples']
 
 
 def check_package(pkgname):
