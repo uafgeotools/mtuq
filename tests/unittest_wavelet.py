fedb7be472e4aa628e512497104c8b47bdc366ed
Ryan <rmodrak@gmail.com>
2019-08-07 20:21:03 -0800

    Fixed broken unit tests

--- tests/unittest_wavelet.py
+++ tests/unittest_wavelet.py
@@ -25,28 +25,28 @@ class TestWavelets(unittest.TestCase):
 
     def test_normalization_Gaussian(self):
         wavelet = Gaussian(sigma=1.)
-        w = wavelet.evaluate_on_interval(half_duration=10., nt=10000)
+        w = wavelet._evaluate_on_interval(half_duration=10., nt=10000)
         dt = (20./(10000-1))
         assert _is_close( dt*np.sum(w), 1. )
 
 
     def test_normalization_Triangle(self):
         wavelet = Triangle(half_duration=1.)
-        w = wavelet.evaluate_on_interval(half_duration=1., nt=1000)
+        w = wavelet._evaluate_on_interval(half_duration=1., nt=1000)
         dt = (2./(1000-1))
         assert _is_close( dt*np.sum(w), 1. )
 
 
     def test_normalization_Trapezoid(self):
         wavelet = Trapezoid(half_duration=1., rise_time=0.5)
-        w = wavelet.evaluate_on_interval(half_duration=1., nt=1000)
+        w = wavelet._evaluate_on_interval(half_duration=1., nt=1000)
         dt = (2./(1000-1))
         assert _is_close( dt*np.sum(w), 1. )
 
 
     def test_normalization_EarthquakeTrapezoid(self):
         wavelet = EarthquakeTrapezoid(rupture_time=0.5, rise_time=0.5)
-        w = wavelet.evaluate_on_interval(half_duration=1., nt=1000)
+        w = wavelet._evaluate_on_interval(half_duration=1., nt=1000)
         dt = (2./(1000-1))
         assert _is_close( dt*np.sum(w), 1. )
 
