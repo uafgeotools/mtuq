6bdea25498a3eb6a3c4e6b5e67766793bbe55a53
rmodrak <rmodrak@gmail.com>
2020-02-20 21:02:06 -0900

    Prototyped lune plotting utilities

--- mtuq/graphics/lune.py
+++ mtuq/graphics/lune.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import pyplot
 from xarray import DataArray
 from mtuq.grid import Grid
+from mtuq.util.lune import to_delta_gamma
+from mtuq.util.xarray import dataarray_to_table
 
 
 def plot_vw(filename, grid, values):
@@ -34,4 +36,48 @@ def plot_vw(filename, grid, values):
     pyplot.ylim([-3./8.*np.pi, 3./8.*np.pi])
 
     pyplot.savefig(filename)
+
+
+def plot_vw_gmt(filename, grid, values):
+
+    da = grid.as_dataarray(values)
+
+    da = da.sum('rho')
+    da = da.min(['kappa', 'sigma', 'h'])
+
+    # normalize values
+    da.values -= da.values.min()
+    da.values /= da.values.max()
+
+    np.savetxt('tmp.vw', dataarray_to_table(da, ('v', 'w')))
+
+
+def plot_lune_gmt(filename, grid, values):
+
+    da = grid.as_dataarray(values)
+
+    da = da.sum('rho')
+    da = da.min(['kappa', 'sigma', 'h'])
+
+    v = np.array(da.coords['v'])
+    w = np.array(da.coords['w'])
+
+    print('v', v.min(), v.max())
+    print('w', w.min(), w.max())
+
+    delta, gamma = to_delta_gamma(v, w)
+
+    # normalize values
+    da.values -= da.values.min()
+    da.values /= da.values.max()
+
+    # flatten values
+    delta, gamma = np.meshgrid(delta, gamma)
+    delta = delta.flatten()
+    gamma = gamma.flatten()
+    values = da.values.flatten()
+
+    np.savetxt('tmp.lune', np.column_stack(
+        [gamma, delta, values]))
+
 
