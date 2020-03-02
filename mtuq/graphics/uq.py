e84fd372329db1781c26af0c924725bdf19a5190
rmodrak <rmodrak@gmail.com>
2020-03-02 14:11:26 -0900

    Further developed plot_likelihood

--- mtuq/graphics/uq.py
+++ mtuq/graphics/uq.py
@@ -3,8 +3,9 @@ import numpy as np
 
 from matplotlib import pyplot
 from xarray import DataArray
-from mtuq.grid import Grid
+from mtuq.grid import Grid, UnstructuredGrid
 from mtuq.util.lune import to_delta, to_gamma
+from mtuq.util.math import closed_interval, open_interval
 from mtuq.util.xarray import dataarray_to_table
 
 
@@ -91,12 +92,23 @@ def plot_misfit_vw(filename, grid, values):
     pyplot.savefig(filename)
 
 
-def plot_likelihood_vw(filename, grid, values):
+def plot_likelihood_vw(filename, grid, values=None):
     """ Plots values on moment tensor grid using v-w projection
     (matplotlib implementation)
     """
-    da = check_grid('FullMomentTensor', grid, values)
+    try:
+        da = check_grid('FullMomentTensorRegular', grid, values)
+        _plot_likelihood_vw_regular(filename, da)
 
+    except:
+        df = check_grid('FullMomentTensorRandom', grid, values)
+        _plot_likelihood_vw_random(filename, df)
+
+
+def _plot_likelihood_vw_regular(filename, da):
+    """ Plots values on moment tensor grid using v-w projection
+    (matplotlib implementation)
+    """
     # manipulate DataArray
     da = da.min(dim='rho')
     da = da.min(dim=('kappa', 'sigma', 'h'))
@@ -109,18 +121,71 @@ def plot_likelihood_vw(filename, grid, values):
     pyplot.ylim([-3./8.*np.pi, 3./8.*np.pi])
 
 
+def _plot_likelihood_vw_random(filename, df, npts_v=20, npts_w=40):
+    """ Plots values on moment tensor grid using v-w projection
+    (matplotlib implementation)
+    """
+    # define edges of cells
+    v = closed_interval(-1./3., 1./3., npts_v+1)
+    w = closed_interval(-3./8.*np.pi, 3./8.*np.pi, npts_w+1)
+
+    # define centers of cells
+    vp = open_interval(-1./3., 1./3., npts_v)
+    wp = open_interval(-3./8.*np.pi, 3./8.*np.pi, npts_w)
+
+    pyplot.savefig(filename)
+
+
+    # compute marginal distribution for v,w
+    marginal = np.zeros((npts_w, npts_v))
+
+    for _i in range(npts_w):
+        for _j in range(npts_v):
+            # which grid points lie within cell (i,j)?
+            subset = df.loc[
+                df['v'].between(v[_j], v[_j+1]) &
+                df['w'].between(w[_i], w[_i+1])]
+
+            # what are the actual and expected number of grid points?
+            na = len(subset)
+            ne = len(df)/float(npts_v*npts_w)
+
+            # what is the sum over all misfit evaulations?
+            marginal[_i, _j] = subset['values'].sum()
+            marginal[_i, _j] *= ne/na**2
+
+    # plot misfit
+    pyplot.figure(figsize=(2., 7.07))
+    pyplot.pcolor(vp, wp, marginal)
+    pyplot.axis('equal')
+    pyplot.xlim([-1./3., 1./3.])
+    pyplot.ylim([-3./8.*np.pi, 3./8.*np.pi])
+
+    pyplot.savefig(filename)
+
+
 def check_grid(grid_type, grid, values):
-    if grid_type in ('DC', 'dc', 'DoubleCouple'):
+    if grid_type in ('DoubleCouple'):
         _check_dc(grid)
 
-    elif grid_type in ('FMT', 'fmt', 'FullMomentTensor'):
+    elif grid_type in ('FullMomentTensor'):
         _check_fmt(grid)
+
+    elif grid_type in ('FullMomentTensorRegular'):
+        _check_fmt(grid)
+
+    elif grid_type in ('FullMomentTensorRandom'):
+        _check_fmt_random(grid)
 
     else:
         raise ValueError("Unexpected grid_type")
 
     if type(grid)==Grid:
         return grid.to_xarray(values)
+
+    elif type(grid)==UnstructuredGrid:
+        return grid.to_dataframe(values)
+
     else:
         return grid
 
@@ -139,3 +204,6 @@ def _check_fmt(grid):
 
 
 
+def _check_fmt_random(grid):
+    for dim in ('rho', 'v', 'w', 'kappa', 'sigma', 'h'):
+        pass # assert dim in grid.dims, Exception("Unexpected grid format")
