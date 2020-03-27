ee5088f1d4f960a59d269be10ff894bcad0ffb45
rmodrak <rmodrak@gmail.com>
2020-03-26 16:52:38 -0800

    New estimate_sigma utility

--- mtuq/misfit/level0.py
+++ mtuq/misfit/level0.py
@@ -102,3 +102,65 @@ def misfit(data, greens, sources, norm, time_shift_groups,
     return results
 
 
+def estimate_sigma(data, greens, best_source, norm, components,
+    time_shift_min, time_shift_max):
+    """ A posteriori standard deviation estimate
+    """
+
+    # error checking
+    assert norm in ('L1', 'L2')
+
+    residuals = []
+    for _j, d in enumerate(data):
+        _components, indices = list_intersect_with_indices(
+            components, get_components(d))
+
+        if not indices:
+            continue
+
+        # generate synthetics
+        greens[_j]._set_components(_components)
+        s = greens[_j].get_synthetics(best_source)
+
+        # time sampling scheme
+        npts = d[0].data.size
+        dt = d[0].stats.delta
+
+        padding_left = int(+time_shift_max/dt)
+        padding_right = int(-time_shift_min/dt)
+        npts_padding = padding_left + padding_right
+
+        # array to hold cross correlations
+        corr = np.zeros(npts_padding+1)
+
+        #
+        # calculate residuals
+        # 
+
+        corr[:] = 0.
+        for _k in indices:
+            corr += np.correlate(s[_k].data, d[_k].data, 'valid')
+
+        npts_shift = padding_left - corr.argmax()
+        time_shift = npts_shift*dt
+
+        # what start and stop indices will correctly shift synthetics
+        # relative to data?
+        start = padding_left - npts_shift
+        stop = start + npts
+
+        for _k in indices:
+
+            # substract data from shifted synthetics
+            r = s[_k].data[start:stop] - d[_k].data
+
+            # sum the resulting residuals
+            if norm=='L1':
+                residuals += [np.sum(np.abs(r))*dt]
+
+            elif norm=='L2':
+                residuals += [np.sum(r**2)*dt]
+
+
+    return np.mean(residuals)**0.5
+
