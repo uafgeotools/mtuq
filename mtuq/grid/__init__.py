4159bc6996369c334d719afa596566f43ddd4fd7
rmodrak <rmodrak@gmail.com>
2020-06-19 14:02:35 -0800

    Reorganized grid modules

---
+++ mtuq/grid/__init__.py
@@ -0,0 +1,8 @@
+
+from mtuq.grid.base import Grid, UnstructuredGrid
+
+from mtuq.grid.moment_tensor import FullMomentTensorGridRegular, FullMomentTensorGridRandom,\
+    DoubleCoupleGridRegular, DoubleCoupleGridRandom
+
+from mtuq.grid.force import ForceGridRegular, ForceGridRandom
+
