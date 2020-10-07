d7ced44fed2ab27642f03a26acdb5f880ed26017
Ryan Modrak <rmodrak@lanl.gov>
2020-10-07 14:12:16 -0600

    Fixed test_misfit docstring

--- tests/test_misfit.py
+++ tests/test_misfit.py
@@ -16,11 +16,32 @@ from mtuq.util.cap import parse_station_codes, Trapezoid
 
 if __name__=='__main__':
     #
-    # Grid search integration test
+    # Checks the correctness of the fast (optimized) misfit function
+    # implementations against a simple pure Python implementation
     #
-    # This script is similar to examples/SerialGridSearch.DoubleCouple.py,
-    # except here we included mangitude and depth and use a coarser grid
+    # In the code, these implementations correspond to the following
     #
+    #   optimization_level=0: simple pure Python
+    #   optimization_level=1: fast pure Python
+    #   optimization_level=2: fast Python/C
+    #
+    # (Note that the `optimization_level` keyword argument does not correspond
+    # at all to C compiler optimization flags.  For example, the NumPy binaries
+    # called by the simple pure Python misfit function are probably compiled 
+    # using a nonzero optimization level?)
+    #
+    # In our own tests, we observe that the two pure Python implementations 
+    # agree almost exactly.
+    #
+    # However, the pure Python and Python/C results may differ by as much as 
+    # 0.1 percent, presumably as a result of differences in the way that
+    # floating-point error accumulates in the sum over residuals.
+    # Further work is required to better understand this issue.
+    #
+    # Possibly relevant is the fact that C extensions are compiled with
+    # `-Ofast` flag, as specified in `setup.py`.
+    #
+
 
     # by default, the script runs with figure generation and error checking
     # turned on
@@ -151,9 +172,9 @@ if __name__=='__main__':
     results_2 = misfit_bw(
         data_bw, greens_bw, grid, optimization_level=2)
 
-    print(results_0.max())
-    print(results_1.max())
-    print(results_2.max())
+    print('  optimization level 0:', results_0.min())
+    print('  optimization level 1:', results_1.min())
+    print('  optimization level 2:', results_2.min())
     print('')
 
 
@@ -168,8 +189,8 @@ if __name__=='__main__':
     results_2 = misfit_sw(
         data_sw, greens_sw, grid, optimization_level=2)
 
-    print(results_0.max())
-    print(results_1.max())
-    print(results_2.max())
+    print('  optimization level 0:', results_0.min())
+    print('  optimization level 1:', results_1.min())
+    print('  optimization level 2:', results_2.min())
     print('')
 
