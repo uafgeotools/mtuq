17534684f56f3bbc1ed3e997426efebecfeed0ed
Ryan Modrak <rmodrak@lanl.gov>
2020-10-13 11:16:49 -0600

    Tweaked docstring formatting

--- mtuq/io/clients/AxiSEM_NetCDF.py
+++ mtuq/io/clients/AxiSEM_NetCDF.py
@@ -59,8 +59,13 @@ class Client(ClientBase):
         Returns a ``GreensTensorList`` in which each element corresponds to a
         (station, origin) pair from the given lists
 
-        :param stations: List of ``mtuq.Station`` objects
-        :param origins: List of ``mtuq.Origin`` objects
+        .. rubric :: Input arguments
+
+        ``stations`` (`list` of `mtuq.Station` objects)
+
+        ``origins`` (`list` of `mtuq.Origin` objects)
+
+        ``verbose`` (`bool`)
         """
         return super(Client, self).get_greens_tensors(stations, origins, verbose)
 
