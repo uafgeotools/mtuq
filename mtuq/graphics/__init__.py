f726d3153fa2e74505dfc9d07367cfe00ba4acbc
rmodrak <rmodrak@gmail.com>
2020-10-19 19:35:48 -0800

    Reorganized graphics.uq modules

--- mtuq/graphics/__init__.py
+++ mtuq/graphics/__init__.py
@@ -1,14 +1,25 @@
 
-from mtuq.graphics.beachball import plot_beachball, misfit_vs_depth
+from mtuq.graphics.beachball import\
+    plot_beachball, misfit_vs_depth
 
-from mtuq.graphics.uq.lune import plot_misfit_lune, plot_likelihood_lune, plot_marginal_lune
-from mtuq.graphics.uq.double_couple import plot_misfit_dc, plot_likelihood_dc, plot_marginal_dc
-from mtuq.graphics.uq.vw import plot_misfit_vw, plot_likelihood_vw, plot_marginal_vw
-from mtuq.graphics.uq.force import plot_misfit_force, plot_likelihood_force, plot_marginal_force
-from mtuq.graphics.uq.depth import plot_misfit_depth, plot_likelihood_depth, plot_marginal_depth
-from mtuq.graphics.uq.hypocenter import plot_misfit_xy, plot_mt_xy
+from mtuq.graphics.uq.lune import\
+    plot_misfit_lune, plot_likelihood_lune, plot_marginal_lune
 
+from mtuq.graphics.uq.double_couple import\
+    plot_misfit_dc, plot_likelihood_dc, plot_marginal_dc
 
-from mtuq.graphics.waveform import plot_data_synthetics, plot_data_greens
+from mtuq.graphics.uq.vw import\
+    plot_misfit_vw, plot_likelihood_vw, plot_marginal_vw
 
+from mtuq.graphics.uq.force import\
+    plot_misfit_force, plot_likelihood_force, plot_marginal_force
+
+from mtuq.graphics.uq.origin_depth import\
+    plot_misfit_depth, plot_likelihood_depth, plot_marginal_depth
+
+from mtuq.graphics.uq.origin_xy import\
+    plot_misfit_xy, plot_mt_xy
+
+from mtuq.graphics.waveform import\
+    plot_data_synthetics, plot_data_greens
 
