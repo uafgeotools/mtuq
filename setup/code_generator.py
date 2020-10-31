0675ce2751451b0b65fd171342ec87c199220755
rmodrak <rmodrak@gmail.com>
2020-10-30 21:27:34 -0800

    Use L2 norm in examples

--- setup/code_generator.py
+++ setup/code_generator.py
@@ -355,12 +355,14 @@ MisfitComments="""
 
 MisfitDefinitions="""
     misfit_bw = Misfit(
+        norm='L2',
         time_shift_min=-2.,
         time_shift_max=+2.,
         time_shift_groups=['ZR'],
         )
 
     misfit_sw = Misfit(
+        norm='L2',
         time_shift_min=-10.,
         time_shift_max=+10.,
         time_shift_groups=['ZR','T'],
