d0c13e9330ff24e156f7b1ee3f932efbe3fe0961
thurinj <julienthurin06@gmail.com>
2021-01-08 16:36:43 +0100

    Fixed error in Magnitude - Depth example.

--- examples/GridSearch.DoubleCouple+Magnitude+Depth.py
+++ examples/GridSearch.DoubleCouple+Magnitude+Depth.py
@@ -18,13 +18,13 @@ from mtuq.util.cap import parse_station_codes, Trapezoid
 if __name__=='__main__':
     #
     # Carries out grid search over source orientation, magnitude, and depth
-    #   
+    #
     # USAGE
     #   mpirun -n <NPROC> python GridSearch.DoubleCouple+Magnitude+Depth.py
     #
     # This is the most complicated example. For a much simpler one, see
     # SerialGridSearch.DoubleCouple.py
-    #   
+    #
 
 
     #
@@ -108,7 +108,7 @@ if __name__=='__main__':
 
     depths = np.array(
          # depth in meters
-        [25000, 30000, 35000, 40000,                    
+        [25000, 30000, 35000, 40000,
          45000, 50000, 55000, 60000])
 
     origins = []
@@ -124,8 +124,8 @@ if __name__=='__main__':
 
     magnitudes = np.array(
          # moment magnitude (Mw)
-        [4.3, 4.4, 4.5,     
-         4.6, 4.7, 4.8]) 
+        [4.3, 4.4, 4.5,
+         4.6, 4.7, 4.8])
 
     grid = DoubleCoupleGridRegular(
         npts_per_axis=30,
@@ -146,10 +146,10 @@ if __name__=='__main__':
 
     if rank==0:
         print('Reading data...\n')
-        data = read(path_data, format='sac', 
+        data = read(path_data, format='sac',
             event_id=event_id,
             station_id_list=station_id_list,
-            tags=['units:cm', 'type:velocity']) 
+            tags=['units:cm', 'type:velocity'])
 
 
         data.sort_by_distance()
@@ -226,11 +226,11 @@ if __name__=='__main__':
         print('Saving results...\n')
 
         plot_data_greens(event_id+'_waveforms.png',
-            [data_bw, data_sw], [greens_bw, greens_sw], 
-            [process_bw, process_sw], [misfit_bw, misfit_sw], 
+            [data_bw, data_sw], [greens_bw, greens_sw],
+            [process_bw, process_sw], [misfit_bw, misfit_sw],
             stations, best_origin, best_source, lune_dict)
 
         plot_misfit_depth(event_id+'_misfit_depth.png',
-            results, origins, grid, event_id)
+            results, origins, grid, title='event_id')
 
         print('\nFinished\n')
