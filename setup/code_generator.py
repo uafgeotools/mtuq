9142319be3b72fc9fb94c3e96cb8d7a972bd85a8
Ryan Modrak <rmodrak@gmail.com>
2021-09-11 20:09:35 -0600

    Expanded detailed analysis example

--- setup/code_generator.py
+++ setup/code_generator.py
@@ -21,17 +21,31 @@ from mtuq.util.cap import parse_station_codes, Trapezoid
 Docstring_DetailedAnalysis_FullMomentTensor="""
 if __name__=='__main__':
     #
-    # Carries out grid search over all moment tensor parameters except
-    # magnitude and peforms detailed analysis of
+    # Peforms detailed analysis involving
     #
-    # - body wave, Rayleigh wave and Love wave data variance
+    # - grid search over all moment tensor parameters except magnitude
+    # - separate body wave, Rayleigh wave and Love wave data categories
+    # - data variance estimation and likelihood analysis
+    #
+    #
+    # Generates figures of
+    #
     # - maximum likelihood surfaces
     # - marginal likelihood surfaces
+    # - misfit surfaces
     # - geographic variation of time shifts and amplitude ratios
+    #
     #
     # USAGE
     #   mpirun -n <NPROC> python DetailedAnalysis.FullMomentTensor.py
     #   
+    #
+    # For simpler examples, see SerialGridSearch.DoubleCouple.py and
+    # GridSearch.FullMomentTensor.py
+    #
+    # For ideas on applying this type of analysis to entire sets of events,
+    # see github.com/rmodrak/mtbench
+    #
 
 """
 
@@ -881,30 +895,47 @@ Main_TestMisfit="""
 
 
 WrapUp_DetailedAnalysis_FullMomentTensor="""
-    #
-    # Data variance estimation and likelihood analysis
-    #
-
     if comm.rank==0:
 
-        # TODO - replace dummy values
-        dummy_bw = 1.e-10
-        dummy_rayleigh = 1.e-10
-        dummy_love = 1.e-10
+        results_sum = results_bw + results_rayleigh + results_love
 
-        results = results_bw + results_rayleigh + results_love
+        #
+        # Data variance estimation and likelihood analysis
+        #
 
-    #
-    # Generate figures and save results
-    #
-
-    if comm.rank==0:
-
-        # source corresponding to minimum misfit
-        idx = results.idxmin('source')
+        # use minimum misfit as initial guess for maximum likelihood
+        idx = results_sum.idxmin('source')
         best_source = grid.get(idx)
         lune_dict = grid.get_dict(idx)
         mt_dict = grid.get(idx).as_dict()
+
+
+        sigma_bw = estimate_sigma(data_bw, greens_bw,
+            best_source, misfit_bw.norm, ['Z', 'R'],
+            misfit_bw.time_shift_min, misfit_bw.time_shift_max)
+
+        sigma_rayleigh = estimate_sigma(data_sw, greens_sw,
+            best_source, misfit_rayleigh.norm, ['Z', 'R'],
+            misfit_rayleigh.time_shift_min, misfit_rayleigh.time_shift_max)
+
+        sigma_love = estimate_sigma(data_sw, greens_sw,
+            best_source, misfit_love.norm, ['T'],
+            misfit_love.time_shift_min, misfit_love.time_shift_max)
+
+
+        print('  Body wave variance estimate:      %.3e' %
+            sigma_bw**2)
+        print('  Rayleigh wave variance estimate:  %.3e' %
+            sigma_rayleigh**2)
+        print('  Love wave variance estimate:      %.3e' %
+            sigma_love**2)
+
+        print()
+
+
+        #
+        # Generate figures and save results
+        #
 
         components_bw = data_bw.get_components()
         components_sw = data_sw.get_components()
@@ -972,14 +1003,17 @@ WrapUp_DetailedAnalysis_FullMomentTensor="""
 
         os.makedirs(event_id+'FMT_likelihood', exist_ok=True)
 
-        plot_likelihood_lune(event_id+'FMT_likelihood/bw.png', results_bw,
-            var=dummy_bw, title='Body wave likelihood\\n(sigma = %.3e)' % dummy_bw)
+        plot_likelihood_lune(event_id+'FMT_likelihood/bw.png',
+            results_bw, var=sigma_bw**2, 
+            title='Body wave likelihood')
 
-        plot_likelihood_lune(event_id+'FMT_likelihood/rayleigh.png', results_rayleigh,
-            var=dummy_rayleigh, title='Rayleigh wave likelihood\\n(sigma = %.3e)' % dummy_rayleigh)
+        plot_likelihood_lune(event_id+'FMT_likelihood/rayleigh.png',
+            results_rayleigh, var=sigma_rayleigh**2, 
+            title='Rayleigh wave likelihood')
 
-        plot_likelihood_lune(event_id+'FMT_likelihood/love.png', results_love,
-            var=dummy_love, title='Love wave likelihood\\n(sigma = %.3e)' % dummy_love)
+        plot_likelihood_lune(event_id+'FMT_likelihood/love.png',
+            results_love, var=sigma_love**2, 
+            title='Love wave likelihood')
 
         print()
 
@@ -1005,27 +1039,27 @@ WrapUp_DetailedAnalysis_FullMomentTensor="""
         print('\\nSaving results...\\n')
 
         # save best-fitting source
-        save_json(event_id+'DC_mt.json', mt_dict)
-        save_json(event_id+'DC_lune.json', lune_dict)
+        save_json(event_id+'FMT_mt.json', mt_dict)
+        save_json(event_id+'FMT_lune.json', lune_dict)
 
 
         # save time shifts and other attributes
-        os.makedirs(event_id+'DC_attrs', exist_ok=True)
+        os.makedirs(event_id+'FMT_attrs', exist_ok=True)
 
-        save_json(event_id+'DC_attrs/bw.json', dict_bw)
-        save_json(event_id+'DC_attrs/sw.json', dict_sw)
+        save_json(event_id+'FMT_attrs/bw.json', dict_bw)
+        save_json(event_id+'FMT_attrs/sw.json', dict_sw)
 
 
         # save processed waveforms as binary files
-        os.makedirs(event_id+'DC_waveforms', exist_ok=True)
+        os.makedirs(event_id+'FMT_waveforms', exist_ok=True)
 
-        data_bw.write(event_id+'DC_waveforms/dat_bw.p')
-        data_sw.write(event_id+'DC_waveforms/dat_sw.p')
+        data_bw.write(event_id+'FMT_waveforms/dat_bw.p')
+        data_sw.write(event_id+'FMT_waveforms/dat_sw.p')
 
-        synthetics_bw.write(event_id+'DC_waveforms/syn_bw.p')
-        synthetics_sw.write(event_id+'DC_waveforms/syn_sw.p')
+        synthetics_bw.write(event_id+'FMT_waveforms/syn_bw.p')
+        synthetics_sw.write(event_id+'FMT_waveforms/syn_sw.p')
 
-        results.save(event_id+'DC_misfit.nc')
+        results_sum.save(event_id+'FMT_misfit/sum.nc')
 
 
         print('\\nFinished\\n')
@@ -1442,6 +1476,8 @@ if __name__=='__main__':
             '    plot_likelihood_lune, plot_marginal_vw,\\\n'+
             '    plot_time_shifts, plot_amplitude_ratios'
             ),
+            'from mtuq.misfit import Misfit',
+            'from mtuq.misfit.waveform import Misfit, estimate_sigma, calculate_norm_data'
             ))
         file.write(Docstring_DetailedAnalysis_FullMomentTensor)
         file.write(Paths_Syngine)
@@ -1464,15 +1500,12 @@ if __name__=='__main__':
             'misfit_sw',
             'misfit_rayleigh',
             ))
-        file.write(
-"""
+        file.write("""
     if comm.rank==0:
         print('Evaluating Love wave misfit...\\n')
 
     results_love = grid_search(
-        data_sw, greens_sw, misfit_love, origin, grid)
-
-"""
+        data_sw, greens_sw, misfit_love, origin, grid)\n"""
         )
         file.write(WrapUp_DetailedAnalysis_FullMomentTensor)
 
