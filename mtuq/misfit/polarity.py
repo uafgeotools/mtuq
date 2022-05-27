aba5476962cf9646bcdf71b9a15f7d36278352ed
thurinj <julienthurin06@gmail.com>
2022-05-27 16:08:34 -0400

    Fixed circular import (moved rad coef computation in util/math)

--- mtuq/misfit/polarity.py
+++ mtuq/misfit/polarity.py
@@ -1,8 +1,12 @@
 
 import numpy as np
 
+import mtuq
+from mtuq.util.math import radiation_coef
 from mtuq.util import Null, iterable, warn
-
+from mtuq.misfit.waveform.level2 import _to_array
+from obspy.taup import TauPyModel
+from mtuq.util.polarity import extract_polarity, extract_takeoff_angle
 
 class PolarityMisfit(object):
     """ Polarity misfit function
@@ -20,7 +24,7 @@ class PolarityMisfit(object):
 
     """
 
-    def __init__(self):
+    def __init__(self, polarity_keyword=None, taup_model='ak135'):
         """ Function handle constructor
         Will be used to manage different kind of polarity input (np.array, pandas dataframe, data with sac header.)
 
@@ -28,74 +32,38 @@ class PolarityMisfit(object):
 
         """
 
-        print('Doing nothing right now')
+        self.polarity_keyword = polarity_keyword
+        self.taup_model = taup_model
 
+        print('Setting up default parameters')
 
-    def __call__(self, data, greens, sources, progress_handle=Null(),
-        set_attributes=False, polarity_keyword = 'user5'):
+    def __call__(self, polarity_input, greens, sources, progress_handle=Null(),
+                 set_attributes=False):
         """ Evaluates polarity misfit on given data.
 
-
         """
-        # First checking that sac headers are set in odrder to run the function
-        # if polarity_keyword not None:
-        if all(d[0].stats.sac != None for d in data):
-            polarities = [d[0].stats.sac[polarity_keyword] for d in data]
-        else:
-            print('SAC format expected')
-            raise NotImplementedError
 
         # Preallocate list containing source vector for faster computation.
-        source_vector_list = np.array([source.as_vector() for source in sources])
-
+        source_vector_list = _to_array(sources)
         # Create the array to store polarity misfit values.
         values = np.zeros((len(sources), 1))
+
         # Extracting measured polarities,  as a numpy array
-        observed_polarities = np.asarray([sta[0].stats.sac[polarity_keyword] for sta in data])
-        # List takeoff_angle and azimuth out of the provided data
-        takeoff_angle = [sta[0].stats.sac['user3'] for sta in data]
-        azimuth = [sta[0].stats.sac['az'] for sta in data]
+        observed_polarities = extract_polarity(polarity_input)
+
+        takeoff_angles = extract_takeoff_angle(greens, taup_model=self.taup_model)
+
+        azimuths = [sta.azimuth for sta in greens]
 
         # Compute a NSOURCESxNSTATIONS array for quick misfit evaluation.
-        predicted_polarity = np.asarray([radiation_coef(source_vector_list, takeoff_angle[i], azimuth[i]) for i in range(len(data))])
+        predicted_polarity = np.asarray(
+            [radiation_coef(source_vector_list, takeoff_angles[i], azimuths[i]) for i in range(len(observed_polarities))])
 
         # Misfit evaluation
         # values = (|obs|*calc) - obs
         # ((|obs|*calc) is used to filter out the un-picked station polarities)
-        values = np.sum(np.abs(predicted_polarity.T*np.abs(observed_polarities)-observed_polarities), axis=1)
+        values = np.sum(np.abs(predicted_polarity.T *
+                               np.abs(observed_polarities)-observed_polarities), axis=1)/2
 
         # returns a NSOURCESx1 np array
         return np.array([values]).T
-
-
-def radiation_coef(mt_array, takeoff_angle, azimuth):
-    """ Computes P-wave radiation coefficient from a collection of sources (2D np.array with source parameter vectors), and the associated takeoff_angle and azimuth for a given station. The radiation coefficient is computed in mtuq orientation conventions.
-
-    Based on Aki & Richards, Second edition (2009), eq. 4.88, p. 108
-    """
-    # Check if mt_array is a 2D array, and make it 2D if not.
-    if len(mt_array.shape) == 1:
-        mt_array = np.array([mt_array])
-
-    alpha = np.deg2rad(takeoff_angle)
-    az = np.deg2rad(azimuth)
-    dir = np.zeros((len(mt_array),3))
-    sth = np.sin(alpha)
-    cth = np.cos(alpha)
-    sphi = np.sin(az)
-    cphi = np.cos(az)
-    dir[:,0]=sth*cphi
-    dir[:,1]=sth*sphi
-    dir[:,2]=cth
-
-    cth = mt_array[:,0]*dir[:,2]*dir[:,2] +\
-          mt_array[:,1]*dir[:,0]*dir[:,0] +\
-          mt_array[:,2]*dir[:,1]*dir[:,1] +\
-       2*(mt_array[:,3]*dir[:,0]*dir[:,2] -\
-          mt_array[:,4]*dir[:,1]*dir[:,2] -\
-          mt_array[:,5]*dir[:,0]*dir[:,1])
-
-    cth[cth>0] = 1
-    cth[cth<0] = -1
-    return cth
-
