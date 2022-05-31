e8287bab67606cac8faf5f99c1b7b135f30f7640
thurinj <julienthurin06@gmail.com>
2022-05-31 11:45:17 -0400

    Finalized PolarityMisfit docstrings instructions.

--- mtuq/misfit/polarity.py
+++ mtuq/misfit/polarity.py
@@ -11,7 +11,7 @@ from mtuq.util.polarity import extract_polarity, extract_takeoff_angle
 class PolarityMisfit(object):
     """ Polarity misfit function
 
-    Compares first-motion polarities between data and synthetics
+    Evaluates first motion polarity by comparing observed and predicted first-motion polarities.
 
     .. rubric:: Usage
 
@@ -20,15 +20,39 @@ class PolarityMisfit(object):
     .. code::
 
         function_handle = Misfit(**parameters)
-        values = function_handle(data, greens, sources)
+        values = function_handle(polarity_input, greens, sources)
+
+    In the first step, the user may supply optional parameters such as the sac
+    header keyword that stores the polarity inputs or the velocity model used
+    to compute takeoff angles (see below for detailed argument descriptions).
+
+    In the second step, the user supplies the polarity inputs (list, numpy
+    array, mtuq Dataset or Green's function list, CAP weight files), Green's
+    functions, and sources.
+    Theoretical first motion polarities are then generated and compared with observed first motions, and a NumPy array of misfit values is returned of
+    the same length as `sources`.
+
+
+    .. rubric:: Parameters
+
+    ``polarity_keyword`` (`str`): SAC header field where observed polarity is
+    stored. Used when the polarity input type is mtuq.Dataset or
+    mtuq.GreensTensorList (for ex: 'user3', would point to
+    trace.stats.sac['user3'] entry in the SAC header).
+
+    ``taup_model`` (`str`): Valid obspy taup model name, used to compute takeoff angles. If not a default obspy.taup model, taup_model should be a path pointing to a valid .npz velocity model file.
+
+    .. note::
+
+    *Convention* : Positive vertical motion should be encoded as +1 and negative vertical motion should be encoded as -1 and unpicked data as 0.
+    (integer values [1, 0, -1])
 
     """
 
     def __init__(self, polarity_keyword=None, taup_model='ak135'):
         """ Function handle constructor
-        Will be used to manage different kind of polarity input (np.array, pandas dataframe, data with sac header.)
-
-        Currently only used to create the polarity misfit object.
+        Pre-set optional arguments for custom polarity misfit inputs (sac
+        header to read polarity from, velocity model to compute takeoff angles).
 
         """
 
