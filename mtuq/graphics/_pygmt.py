76d962283179c1aae42bd14fc9a965012a057001
Ryan Modrak <rmodrak@gmail.com>
2023-03-23 15:41:42 -0600

    Changed force header projection

--- mtuq/graphics/_pygmt.py
+++ mtuq/graphics/_pygmt.py
@@ -19,25 +19,25 @@ def plot_force(filename, force_dict):
     lat = np.degrees(np.pi/2 - np.arccos(force_dict['h']))
     lon = _wrap(force_dict['phi'] + 90.)
 
-    proj_arg="V6i"
+    proj_arg="A0/0/6i"
     area_arg="-180/180/-90/90"
 
     # specify basemap
-    fig.basemap(projection=proj_arg, region=area_arg, frame=['xg180','yg60'])
+    fig.basemap(projection=proj_arg, region=area_arg, frame=['xg180','yg30'])
 
     # plot arcs
-    fig.text(x=90., y=0., text='E', font='40p')
-    fig.plot(x=[90.,90.], y=[90.,7.5], pen='1.5p,0/0/0/35')
-    fig.plot(x=[90.,90.], y=[-90.,-7.5], pen='1.5p,0/0/0/35')
+    fig.text(x=90./2., y=0., text='E', font='40p')
+    fig.plot(x=[90./2.,90./2.], y=[90.,7.5], pen='1.5p,0/0/0/35')
+    fig.plot(x=[90./2.,90./2.], y=[-90.,-7.5], pen='1.5p,0/0/0/35')
     #fig.text(x=0., y=0., text='S', font='40p')
     fig.plot(x=[0.,0.], y=[90.,7.5], pen='1.5p,0/0/0/35')
     fig.plot(x=[0.,0.], y=[-90.,-7.5], pen='1.5p,0/0/0/35')
-    fig.text(x=-90., y=0., text='W', font='40p')
-    fig.plot(x=[-90.,-90.], y=[90.,7.5], pen='1.5p,0/0/0/35')
-    fig.plot(x=[-90.,-90.], y=[-90.,-7.5], pen='1.5p,0/0/0/35')
+    fig.text(x=-90./2., y=0., text='W', font='40p')
+    fig.plot(x=[-90./2.,-90./2.], y=[90.,7.5], pen='1.5p,0/0/0/35')
+    fig.plot(x=[-90./2.,-90./2.], y=[-90.,-7.5], pen='1.5p,0/0/0/35')
 
     # plot force orientation
-    fig.plot(x=lon, y=lat, style="d40p", pen="1p,black", fill="black")
+    fig.plot(x=lon/2., y=lat, style="d40p", pen="1p,black", fill="black")
 
     fig.savefig(filename)
 
