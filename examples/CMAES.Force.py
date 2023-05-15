b1ac1bf99c9ddd76bdbd88c66a4c0619c8cfecbb
thurinj <julienthurin06@gmail.com>
2023-05-15 11:55:51 -0400

    Refined example scripts
    Overhauled the script description and populated with comments and recommendations on uses.

--- examples/CMAES.Force.py
+++ examples/CMAES.Force.py
@@ -3,7 +3,7 @@
 import os
 import numpy as np
 
-from mtuq import read, open_db
+from mtuq import read, open_db, download_greens_tensors
 from mtuq.event import Origin
 from mtuq.misfit import Misfit
 from mtuq.process_data import ProcessData
@@ -16,19 +16,35 @@ from mtuq.stochastic_sampling.cmaes_parallel import parallel_CMA_ES
 
 if __name__=='__main__':
     #
-    # Carries out grid search over all moment tensor parameters
+    # Carries out CMA-ES inversion over moment tensor parameters
     #
     # USAGE
-    #   mpirun -n <NPROC> python GridSearch.FullMomentTensor.py
-    #   
-
+    #   mpirun -n <NPROC> python CMAES.Force.py
+    # ---------------------------------------------------------------------
+    # The code is intended to be run in parallel, although the `greens` mode
+    # exhibits some scaling issues (potentially due to IO and MPI comm overheads).
+    # ---------------------------------------------------------------------
+    # The `greens` mode with 24 ~ 120 mutants per generation (CMAES parameter 'lambda')
+    # should only take a few minutes to run on a single core, and achieves better 
+    # results than when using a grid search. (No restriction of being on a grid, including 
+    # finer Mw search).
+    # The algorithm can converge with as low as 6 mutants per generation, but this is
+    # not recommended as it will take a longer time to converge, and is more prone to
+    # getting stuck in local minima. This could be useful if you are trying to find
+    # other minima, but is not recommended for general use.
+    # ---------------------------------------------------------------------
+    # The 'database' should be used when searching over depth / hypocenter.
+    # I recommend anything between 24 to 120 mutants per generation, (CMAES parameter 'lambda')
+    # Each mutant will require its own greens functions, meaning the most compute time will be
+    # spent fetching and pre-processing greens functions. This can be sped up by using a
+    # larger number of cores, but the scaling is not perfect. (e.g. 24 cores is not 24x faster)
+    # ---------------------------------------------------------------------
 
     path_data=    fullpath('data/examples/20090407201255351/*.[zrt]')
     path_weights= fullpath('data/examples/20090407201255351/weights.dat')
     event_id=     '20090407201255351'
     model=        'ak135'
     mode =        'greens' # 'database' or 'greens'
-    mode =        'database' # 'database' or 'greens'
 
     #
     # Body and surface wave measurements will be made separately
@@ -79,14 +95,15 @@ if __name__=='__main__':
 
     #
     # User-supplied weights control how much each station contributes to the
-    # objective function
+    # objective function. Note that these should be functional in the CMAES
+    # mode.
     #
 
     station_id_list = parse_station_codes(path_weights)
 
 
     #
-    # Next, we specify the moment tensor grid and source-time function
+    # Next, we specify the source wavelet. 
     #
 
     wavelet = Trapezoid(
@@ -94,8 +111,9 @@ if __name__=='__main__':
 
 
     #
-    # Origin time and location will be fixed. For an example in which they 
-    # vary, see examples/GridSearch.DoubleCouple+Magnitude+Depth.py
+    # The Origin time and hypocenter are defined as in the grid-search codes
+    # It will either be fixed and used as-is by the CMA-ES mode (typically `greens` mode)
+    # or will be used as a starting point for hypocenter search (using the `database` mode)
     #
     # See also Dataset.get_origins(), which attempts to create Origin objects
     # from waveform metadata
@@ -134,10 +152,14 @@ if __name__=='__main__':
         data_sw = data.map(process_sw)
 
 
-        print('Reading Greens functions...\n')
         if mode == 'greens':
-            db = open_db('/Users/julienthurin/Downloads/model/ak135f/', format='AxiSEM', include_mt=False, include_force=True)
-            greens = db.get_greens_tensors(stations, origin, model)
+            print('Reading Greens functions...\n')
+            greens = download_greens_tensors(stations, origin, model, include_mt=False, include_force=True)
+            # ------------------
+            # Alternatively, if you have a local AxiSEM database, you can use:
+            # db = open_db('/Users/julienthurin/Downloads/model/ak135f/', format='AxiSEM', include_mt=False, include_force=True)
+            # greens = db.get_greens_tensors(stations, origin, model)
+            # ------------------
             greens.convolve(wavelet)
             greens_bw = greens.map(process_bw)
             greens_sw = greens.map(process_sw)
@@ -160,6 +182,7 @@ if __name__=='__main__':
         greens_bw = comm.bcast(greens_bw, root=0)
         greens_sw = comm.bcast(greens_sw, root=0)
     elif mode == 'database':
+        # This mode expects the path to a local AxiSEM database to be specified 
         db = open_db('/Users/julienthurin/Downloads/model/ak135f/', format='AxiSEM', include_mt=False, include_force=True)
 
     #
@@ -174,10 +197,20 @@ if __name__=='__main__':
     PROCESS = [process_bw, process_sw]
     MISFIT = [misfit_bw, misfit_sw]
 
-    CMA = parallel_CMA_ES(parameter_list , origin=origin, lmbda=24)
-    CMA.sigma = 1
+    popsize = 24 # -- CMA-ES population size (you can play with this value)
+    CMA = parallel_CMA_ES(parameter_list , origin=origin, lmbda=popsize)
+    CMA.sigma = 1 # -- CMA-ES step size, defined as 1 standard deviation of the initial parameter distribution (you can play with this value)
     iter = 120
     for i in range(iter):
+        # ------------------
+        # At the moment the full CMA-ES algorithm is executed in this loop.
+        # In the future, we will add a 'Solve' method to the CMA_ES class
+        # that will perform the optimisation in a single call given some 
+        # input parameters.
+        # 
+        # Algorithm is described in:
+        # Hansen, N. (2016) The CMA Evolution Strategy: A Tutorial. arXiv:1604.00772
+        # ------------------
         if comm.rank==0:
             print('Iteration %d\n' % i)
         CMA.draw_mutants()
