3c402a9cfacc31d7639ee2221a06b7c3cd7fe27c
thurinj <julienthurin06@gmail.com>
2023-08-10 15:12:40 -0400

    Update CMAES.FullMomentTensor.py
    Remodeled the example script, and modified plotting function.

--- examples/CMAES.FullMomentTensor.py
+++ examples/CMAES.FullMomentTensor.py
@@ -26,6 +26,19 @@ def plot_lune(CMA):
     '''
 
     A = CMA.mutants_logger_list
+
+    # Check if key v is present in ds_lune, else add a column of same length as the other columns and fill it with zeros.
+    # Make it so that the v column is the 2nd column in the DataArray or DataFrame.
+    if "v" not in A:
+        A["v"] = 0
+        A = A[["Mw", "v", "kappa", "sigma", "h", "misfit"]]
+
+    # Check if key w is present in ds_lune, else add a column of same length as the other columns and fill it with zeros.
+    # Make it so that the w column is the 3rd column in the DataArray or DataFrame.
+    if "w" not in A:
+        A["w"] = 0
+        A = A[["Mw", "v", "w", "kappa", "sigma", "h", "misfit"]]
+
     LIST = A.copy()
     v = LIST['v']
     w = LIST['w']
@@ -35,8 +48,8 @@ def plot_lune(CMA):
     plt.scatter(to_gamma(v), to_delta(w), c=np.log(m))
     plt.scatter(to_gamma(V), to_delta(W), c='red', marker='x', zorder=10000000)
 
-    # Make sure the colorscale includes the 80% data percentile
-    plt.clim(np.log(np.percentile(m, 50)))
+    # Make sure the colorscale includes the 75% data percentile
+    plt.clim(np.log(np.percentile(m, 75)))
 
     plt.show()
 
@@ -56,12 +69,12 @@ if __name__=='__main__':
     # results than when using a grid search. (No restriction of being on a grid, including 
     # finer Mw search).
     # The algorithm can converge with as low as 6 mutants per generation, but this is
-    # not recommended as it will take a longer time to converge, and is more prone to
+    # not recommended as it will take more steps to converge, and is more prone to
     # getting stuck in local minima. This could be useful if you are trying to find
     # other minima, but is not recommended for general use.
     # ---------------------------------------------------------------------
     # The 'database' should be used when searching over depth / hypocenter.
-    # I recommend anything between 24 to 120 mutants per generation, (CMAES parameter 'lambda')
+    # I also recommend anything between 24 to 120 mutants per generation, (CMAES parameter 'lambda')
     # Each mutant will require its own greens functions, meaning the most compute time will be
     # spent fetching and pre-processing greens functions. This can be sped up by using a
     # larger number of cores, but the scaling is not perfect. (e.g. 24 cores is not 24x faster)
@@ -78,8 +91,7 @@ if __name__=='__main__':
     path_weights= fullpath('data/examples/20090407201255351/weights.dat')
     event_id=     '20090407201255351'
     model=        'ak135'
-    mode =        'database' # 'database' or 'greens'
-    # mode =        'database' # 'database' or 'greens'
+    mode =        'greens' # 'database' or 'greens'
 
     #
     # Body and surface wave measurements will be made separately
@@ -208,6 +220,7 @@ if __name__=='__main__':
             db = None
             greens_bw = None
             greens_sw = None
+            greens = None
 
     stations = comm.bcast(stations, root=0)
     data_bw = comm.bcast(data_bw, root=0)
@@ -216,16 +229,17 @@ if __name__=='__main__':
     if mode == 'greens':
         greens_bw = comm.bcast(greens_bw, root=0)
         greens_sw = comm.bcast(greens_sw, root=0)
+        greens = comm.bcast(greens, root=0)
     elif mode == 'database':
         # This mode expects the path to a local AxiSEM database to be specified 
-        db = open_db('/Path/to/Axisem/Database/ak135f/', format='AxiSEM')
+        db = open_db('/Users/julienthurin/Downloads/model/ak135f/', format='AxiSEM')
 
     #
     # The main computational work starts now
     #
     
     # Defining source type (full moment tensor, deviatoric moment tensor or double couple)
-    src_type = 'dc' # 'full', 'deviatoric' or 'dc'
+    src_type = 'full' # 'full', 'deviatoric' or 'dc'
 
     if mode == 'database':
         # For a full search with depth, latitutde and longitude, use:
@@ -235,51 +249,65 @@ if __name__=='__main__':
     elif mode == 'greens':
         parameter_list = initialize_mt(Mw_range=[4,6], src_type=src_type)
 
-    # Creating list of important objects to be passed to plotting function later
-    DATA = [data_bw, data_sw]
-    PROCESS = [process_bw, process_sw]
-    MISFIT = [misfit_bw, misfit_sw]
+    # Creating list of important objects to be passed to solving and plotting functions later
+    DATA = [data_bw, data_sw]  # add more as needed
+    MISFIT = [misfit_bw, misfit_sw]  # add more as needed
+    PROCESS = [process_bw, process_sw]  # add more as needed
+    GREENS = [greens_bw, greens_sw] if mode == 'greens' else None  # add more as needed
 
     popsize = 24 # -- CMA-ES population size - number of mutants (you can play with this value, 24 to 120 is a good range)
     CMA = parallel_CMA_ES(parameter_list , origin=origin, lmbda=popsize)
     CMA.sigma = 5.0 # -- Initial standard deviation (4 ~ 5 seems to provide a balanced exploration/exploitation and avoid getting stuck in local minima)
     iter = 120 # -- Number of iterations (you can play with this value, 120 to 240 is a good range)
-    for i in range(iter):
-        # ------------------
-        # At the moment the full CMA-ES algorithm is executed in this loop.
-        # In the future, we will add a 'Solve' method to the CMA_ES class
-        # that will perform the optimisation in a single call given some 
-        # input parameters.
-        # 
-        # Algorithm is described in:
-        # Hansen, N. (2016) The CMA Evolution Strategy: A Tutorial. arXiv:1604.00772
-        # ------------------
-        if comm.rank==0:
-            print('Iteration %d\n' % i)
-        CMA.draw_mutants()
-        if mode == 'database':
-            # It using the database mode, the catalog origin and process functions are required.
-            # As with the grid-search, we can separate Body-wave and Surface waves misfit. It is also possible to
-            # Split the misfit into different time-shift groups (e.g. b-ZR, s-ZR, s-T, etc.)
-            mis_bw = CMA.eval_fitness(data_bw, stations, misfit_bw, db, origin,  process_bw, wavelet, verbose=False)
-            mis_sw = CMA.eval_fitness(data_sw, stations, misfit_sw, db, origin,  process_sw, wavelet, verbose=False)
-        elif mode == 'greens':
-            mis_bw = CMA.eval_fitness(data_bw, stations, misfit_bw, greens_bw)
-            mis_sw = CMA.eval_fitness(data_sw, stations, misfit_sw, greens_sw)
-        CMA.gather_mutants()
-        CMA.fitness_sort(mis_bw+mis_sw)
-        CMA.update_mean()
-        CMA.update_step_size()
-        CMA.update_covariance()
 
-        # if i = 0 or multiple of 10 and Last iteration:
-        if i == 0 or i % 10 == 0 or i == iter-1:
-            if mode == 'database':
-                CMA.plot_mean_waveforms(DATA, PROCESS, MISFIT, stations, db)
-            elif mode == 'greens':
-                CMA.plot_mean_waveforms(DATA, PROCESS, MISFIT, stations, db=greens)
-            
-            if src_type == 'full' or src_type == 'deviatoric' or src_type == 'dc':
-                if comm.rank==0:
-                    result = CMA.mutants_logger_list
-                    plot_combined('combined.png', result, colormap='viridis')
+    if mode == 'database':
+        CMA.Solve(DATA, stations, MISFIT, PROCESS, db, iter, wavelet, plot_interval=10, max_iter=iter)
+    elif mode == 'greens':
+        CMA.Solve(DATA, stations, MISFIT, PROCESS, GREENS, iter, plot_interval=10, max_iter=iter)
+
+    result = CMA.mutants_logger_list # -- This is the list of mutants (i.e. the population) at each iteration
+    # This is a mtuq.grid_search.MTUQDataFrame object, which is the same as when conducting a random grid-search
+    # It is therefore compatible with the "regular" plotting functions in mtuq.graphics 
+    fig = CMA._scatter_plot() # -- This is a scatter plot of the mutants at the last iteration
+    fig.savefig(event_id+'CMA-ES_final_step.png')
+
+
+    # ================================================================================================
+    # FOR EDUCATIONAL PURPOSE -- This is what is happening under the hood in the Solve function
+    # ================================================================================================
+    # for i in range(iter):
+    #     # ------------------
+    #     # The CMA-ES Algorithm is described in:
+    #     # Hansen, N. (2016) The CMA Evolution Strategy: A Tutorial. arXiv:1604.00772
+    #     # ------------------
+    #     CMA.draw_mutants() # -- Draw mutants from the current distribution
+    #     if mode == 'database':
+    #         # It using the database mode, the catalog origin and process functions are required.
+    #         # As with the grid-search, we can separate Body-wave and Surface waves misfit. It is also possible to
+    #         # Split the misfit into different time-shift groups (e.g. b-ZR, s-ZR, s-T, etc.)
+    #         mis_bw = CMA.eval_fitness(data_bw, stations, misfit_bw, db, origin,  process_bw, wavelet, verbose=False)
+    #         mis_sw = CMA.eval_fitness(data_sw, stations, misfit_sw, db, origin,  process_sw, wavelet, verbose=False)
+    #     elif mode == 'greens':
+    #         mis_bw = CMA.eval_fitness(data_bw, stations, misfit_bw, greens_bw)
+    #         mis_sw = CMA.eval_fitness(data_sw, stations, misfit_sw, greens_sw)
+    #
+    #     CMA.gather_mutants() # -- Gather mutants from all processes 
+    #     CMA.fitness_sort(mis_bw+mis_sw) # -- Sort mutants by fitness
+    #     CMA.update_mean() # -- Update the mean of the distribution
+    #     CMA.update_step_size() # -- Update the step size
+    #     CMA.update_covariance()   # -- Update the covariance matrix
+    #
+    #     # ------------------  Plotting results ------------------
+    #     # if i = 0 or multiple of 10 and Last iteration:
+    #     if i == 0 or i % 10 == 0 or i == iter-1:
+    #         if mode == 'database':
+    #             CMA.plot_mean_waveforms(DATA, PROCESS, MISFIT, stations, db)
+    #         elif mode == 'greens':
+    #             CMA.plot_mean_waveforms(DATA, PROCESS, MISFIT, stations, db=greens)
+    #
+    #         if src_type == 'full' or src_type == 'deviatoric' or src_type == 'dc':
+    #             if comm.rank==0:
+    #                 result = CMA.mutants_logger_list # This one is an important one! 
+    #                 It returns a DataFrame, the same as when using a random grid search and is therefore compatible with the default mtuq plotting tools.
+    #                 plot_combined('combined.png', result, colormap='viridis')
+    # ================================================================================================
